071c3efdbe17c8832f7a2327ac4392dbc2369af2
Daniela0209 <grajalesdaniela253@gmail.com>
2021-06-13 17:46:17 -0500

    se crearon las carpetas que se van a utilizar

---
+++ src/main/java/com/co/sofka/Perfect_cosmetic/PerfectCosmeticApplication.java
@@ -0,0 +1,13 @@
+package com.co.sofka.Perfect_cosmetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectCosmeticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectCosmeticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/sofka/Perfect_cosmetic/PerfectCosmeticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.sofka.Perfect_cosmetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectCosmeticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
