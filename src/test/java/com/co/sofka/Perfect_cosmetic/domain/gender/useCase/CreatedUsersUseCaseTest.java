09dd5bb7413df25a9ec458daeb9c234d08fdd255
Daniela0209 <grajalesdaniela253@gmail.com>
2021-06-15 15:55:44 -0500

    se realizo el test del caso de uso CreatedUsers

---
+++ src/test/java/com/co/sofka/Perfect_cosmetic/domain/gender/useCase/CreatedUsersUseCaseTest.java
@@ -0,0 +1,38 @@
+package com.co.sofka.Perfect_cosmetic.domain.gender.useCase;
+
+
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.support.RequestCommand;
+import com.co.sofka.Perfect_cosmetic.domain.gender.commands.CreateUsers;
+import com.co.sofka.Perfect_cosmetic.domain.gender.events.UsersCreated;
+import com.co.sofka.Perfect_cosmetic.domain.gender.values.Email;
+import com.co.sofka.Perfect_cosmetic.domain.gender.values.NameUser;
+import com.co.sofka.Perfect_cosmetic.domain.gender.values.UsersId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CreatedUsersUseCaseTest {
+
+    @Test
+
+    public void CreateUsersTest(){
+
+        var command = new CreateUsers(UsersId.of("0"),new NameUser("Daniela"),new Email("Grajalesdaniela253@gmail.com"));
+
+        UsersCreated usersCreated = executeUseCase(command);
+
+        Assertions.assertEquals("0", usersCreated.UsersId().value());
+    }
+
+    private UsersCreated executeUseCase(CreateUsers command){
+        var usecase = new CreatedUsersUseCase();
+
+        var events = UseCaseHandler.getInstance()
+                .syncExecutor(usecase, new RequestCommand<>(command))
+                .orElseThrow()
+                .getDomainEvents();
+        var usersCreated =  (UsersCreated) events.get(0);
+        return usersCreated;
+    }
+
+}
